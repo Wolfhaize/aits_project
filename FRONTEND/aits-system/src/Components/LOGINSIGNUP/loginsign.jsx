607287bf7a84bcd7b58017438a1076b413ca7502
Zoe <zoetino07@gmail.com>
2025-02-17 11:27:57 +0300

    Added an html-page

---
+++ FRONTEND/aits-system/src/Components/LOGINSIGNUP/loginsign.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './loginsign.css'
+
+export const loginsign = () => {
+  return (
+    <div className='container'>
+         <div className='header'>
+            <div className="text">Sign Up</div>
+            <div className='underline'></div>
+         </div>
+         <div className='inputs'>
+            <div className='input'>
+                <input type="text" />
+            
+         </div>
+    
+    </div>
+
+
+
+
+
+        
+
+
+
+
+
+
+
+    
+  )
+}
