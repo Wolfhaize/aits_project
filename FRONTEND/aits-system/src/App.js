a22b60ceb70125121d63e68204b2642d185b3d89
Rita Akankunda <akankundarita04@gmail.com>
2025-02-25 18:39:04 +0300

    Revert "Merge branch 'Calvin'"
    This reverts commit d8586870f350787165e3af20def952e84d22e3c6, reversing
    changes made to 466d7edc15f7773ef49035405de9f22932b5f7be.

---
+++ FRONTEND/aits-system/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { LoginSignup } from './Components/Loginsignup/loginsignup'; // Correct import
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <LoginSignup />
+      
+    </div>
+  );
+}
+
+
+
+export default App; 
