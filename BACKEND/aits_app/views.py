d649309816cc921672f3741eb662adcbb7ff164b
Rita Akankunda <akankundarita04@gmail.com>
2025-02-26 19:05:29 +0300

    adding missing files

--- BACKEND/aits_app/views.py
+++ BACKEND/aits_app/views.py
@@ -1,15 +1,3 @@
-from rest_framework import viewsets
-from .models import Issue, User, Department
-from .serializers import IssueSerializer, DepartmentSerializer, UserSerializer
+from django.shortcuts import render
 
-class IssueViewSet(viewsets.ModelViewSet):
-    queryset = Issue.objects.all()
-    serializer_class = IssueSerializer
-    
-class DepartmentViewSet(viewsets.ModelViewSet):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
-    
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer       
+# Create your views here.
