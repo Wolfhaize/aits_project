d3e9fda249032c1bb5d007da8e8ae8f94e26bf67
khana-kaye <khanakaye4@gmail.com>
2025-03-09 18:30:39 +0300

    updates on signup page
    a few updates on user role selections and others

--- FRONTEND/aits-system/src/App.js
+++ FRONTEND/aits-system/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LoginSignup } from './Components/Loginsignup/loginsignup'; 
+import { LoginSignup } from './Components/Loginsignup/loginsignup';
+import Signup from './Signup/Signup'; 
+
 
 function App() {
   return (
+    <div className = 'App'>
+      <Signup />
+    </div>
+
+    /*
     <Router>
       <Routes>
         <Route path="/" element={<LoginSignup />} />
       </Routes>
     </Router>
+    */
+
+  
   );
 }
 
--- FRONTEND/aits-system/src/Components/Signup/Signup.jsx
+++ FRONTEND/aits-system/src/Components/Signup/Signup.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import React, {useState} from 'react';
+import './Signup.css';
+
+
 
 function SignupPage(){
     /*manage role selection in dropdown box*/
@@ -22,57 +25,129 @@ function SignupPage(){
         setRole( evt.target.value);
      
 
-     /* here, when maybe a student enters in as a lecturer by mistake, then goes back to select student, the information entered in for lecturer erases or becomes empty*/
-
-     setStudentId('');
-     setLecturerId('');
-     setRegistrarId('');
-     };
-
      /* submission of the login form*/
-     const submitLoginForm = (evt) => {
+     const handleSubmit = (evt) => {
         evt.preventDefault();
-        console.log('Form submitted successfully!: ' {
-            
-            role,
-            username,
-            email,
-            password,
-            studentId,
-            lecturerId,
-            registrarId,
-            })
+       
+
+        /*This requires all fields to be filled before procedding with the form submission*/
+        if (!email || !role || !username || !password ){
+          alert('Please fill in the required fields!');
+          return;
+        }
+
+        if ((role == 'student' &&!studentId)||
+            (role == 'lecturer' &&!lecturerId)||
+            (role == 'registrar' &&!registrarId)){
+              alert('Please provide a valid &{role} Id.');
+              return;
+            }
+          }
+
+        /*Handling the submission of data, sending it to the server*/
+        const userData = {
+          role,
+          email,
+          username,
+          password,
+          
+          }
+
+          /* where the server will send the input data*/
+          if (role === 'student') {
+            requestData.id = studentId;
+          } else if (role === 'lecturer') {
+            requestData.id = lecturerId;
+          } else {
+            requestData.id = registrarId;
+          };
+
+          console.log('Form Submitted: ',userData );
+          alert('Congrats! Signup Successful!');
         };
+
+
+
 
       return (
         <div className= 'signUp Page'  >
             <h2>Welcome to the SignUp Page for AITS</h2>
+            {/* <form  onSubmit = {handleSignUp}></form>*/}
+            <form className='signUp form'>
+              <div className='inputArea'>
+                {/* area for the username*/}
+                <label htmlFor="userName">userName:</label>
+                <input 
+                type="text"
+                id='name'
+                placeholder='Enter your username' />
 
-            
-                    
+                {/* area for the email*/}
+                <label htmlFor="email">Email: </label>
+                <input 
+                type="email"
+                id='email'
+                placeholder='Enter your email' />
+
+                {/* area for the password*/}
+                <label htmlFor="password">Password: </label>
+                <input 
+                type="password"
+                id='password'
+                placeholder='Enter your password' />
 
 
+                {/* conditional or user role fields*/}
+                {/*if role is true, then what is in the brackets will be rendered*/}
+                {/*onChange, when the user types in it, it updates the studentId or lecturerId or registrarId state*/}
+                  {role === 'student' &&(
+                    <div>
+                      <label> Student ID</label>
+                      <input 
+                        type="text" 
+                        value = {studentId}
+                        onchange = {(evt) => setstudentId(evt.target.value)}
+                        placeholder = 'Enter your Student ID'
+                        required 
+                        />
+                    </div>
+                  )}
 
-            
-            
+                  {role === 'lecturer' &&(
+                    <div>
+                      <label> lecturer ID</label>
+                      <input 
+                        type="text" 
+                        value = {lecturerId}
+                        onchange = {(evt) => setlecturerId(evt.target.value)}
+                        placeholder = 'Enter your lecturer ID'
+                        required 
+                        />
+                    </div>
+                  )}
 
+                  {role === 'registrar' &&(
+                    <div>
+                      <label> Registrar ID</label>
+                      <input 
+                        type="text" 
+                        value = {registrarId}
+                        onchange = {(evt) => setregistrarId(evt.target.value)}
+                        placeholder = 'Enter your Registrar ID'
+                        required 
+                        />
+                    </div>
+                  )}
 
+                  {/*submit button*/}
+                  <div>
+                    <button type = 'Signup'> Sign Up</button>
 
+                  </div>
 
- 
-
-
-
-
-            </div>
-
-        
-
-      )  
-
-
-     }
-
-}
-
-export default Signup
+              </div>
+            </form>
+          </div>
+      )
+            }
+export default SignupPage;
