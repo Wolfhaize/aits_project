9867167abb6a3655796f1b6ab2a8fd2c04c85a8e
Rita Akankunda <akankundarita04@gmail.com>
2025-03-10 22:11:31 +0300

    views for login and logout

--- BACKEND/accounts/views.py
+++ BACKEND/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
+ # Registration View
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -10,3 +12,21 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+# Login View
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'accounts/login.html')
+
+# Logout View
+def logout_view(request):
+    logout(request)
+    return redirect('login')
