6459b1a35c821d50ca8bf51d0be9fbf35762fb8c
Rita Akankunda <akankundarita04@gmail.com>
2025-03-13 22:30:26 +0300

    serializers.py for sdp app

--- BACKEND/SDPapp/serializers.py
+++ BACKEND/SDPapp/serializers.py
@@ -4,16 +4,16 @@ from .models import CustomUser , Department, Issue
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser 
-        fields = ['id','Password', 'role'] 
+        fields = ['id', 'username', 'email', 'role']
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
-        fields = ['id','description']
+        fields = ['id', 'name', 'description']
 
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
-        fields =['id','category', 'user', 'priority_level', 'description', 'status','assigned_to', 'created_at', 'updated_at']
+        fields =['id','title','category', 'user', 'description', 'status','assigned_to', 'created_at', 'updated_at']
 
         
