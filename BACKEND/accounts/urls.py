6af5e468b6c5afb33189cef2e95fdf71f1bcbeb3
Rita Akankunda <akankundarita04@gmail.com>
2025-03-13 22:26:16 +0300

    urls.py for accounts

--- BACKEND/accounts/urls.py
+++ BACKEND/accounts/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register'),  # Add parentheses to call as_view()
+    path('login/', views.LoginView.as_view(), name='login'),          # Add parentheses to call as_view()
+    path('logout/', views.LogoutView.as_view(), name='logout'),       # Add parentheses to call as_view()
 ]
-
