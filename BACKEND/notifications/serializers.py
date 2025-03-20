8ce80a94b2ac1021d2c0bd3bdbebb37f1c2081f7
Rita Akankunda <akankundarita04@gmail.com>
2025-03-20 08:24:47 +0300

    Update serializers.py

--- BACKEND/notifications/serializers.py
+++ BACKEND/notifications/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from .models import Notification, AuditLog
 
 class NotificationSerializer(serializers.ModelSerializer):
-    Class Meta:
+    class Meta:
         model = Notification
-        fields = ['id', 'user', ' message', 'is_read', 'created_at']
+        fields = ['id', 'user', 'message', 'is_read', 'created_at']
+        read_only_fields = ['user', 'created_at'] 
 
 class AuditLogSerializer(serializers.ModelSerializer):
-    Class Meta:
+    class Meta:
         model = AuditLog
-        fields = ['id', 'user', 'action' 'timestamp']
-        read_only_fields = ['id', 'timestamp']
+        fields = ['id', 'user', 'action', 'timestamp']
+        read_only_fields = ['id', 'timestamp'] 
