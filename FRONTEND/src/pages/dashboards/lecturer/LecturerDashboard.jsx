01ca05b741236b0fed76ed2667d51cc5172a7cae
khana-kaye <khanakaye4@gmail.com>
2025-03-27 00:22:41 +0300

    update lecturer dashboard
    added a search bar, created a header with lecturer name, department, state management for handling search input, logout button that triggers the logout process

--- FRONTEND/src/pages/dashboards/lecturer/LecturerDashboard.jsx
+++ FRONTEND/src/pages/dashboards/lecturer/LecturerDashboard.jsx
@@ -1,5 +1,6 @@
 import DashboardLayout from "../../../layouts/DashboardLayout";
 import IssueCard from "../../../components/IssueCard";
+import { useState } from "react";
 
 
 /*LecturerDashboard component is the main dashboard for lecturers*/
@@ -19,10 +20,42 @@ const LecturerDashboard = ({lecturerName, department}) => {
   const handleLogout = () => {
     console.log('Logging out ...');
   };
-  
+
   return (
     <DashboardLayout role="Lecturer">
-      <h2>Lecturer Dashboard</h2>
+      {/*header section will be displayed at the top of the dashboard */}
+      <header className = 'dashboard-header'>
+        <div className = 'header-container'>
+          <h1 className = 'project-name'>Welcome to AITS</h1>
+
+          {/*lecturer infor, displaying lecturer name and department */}
+          {/*the values are passed in as props */}
+          <div className = 'lecturer-infor'>
+            <span className = 'lecturer-name'>{lecturerName}</span>
+            <span className = 'department-name'>{department}</span>
+          </div>
+
+          {/*search bar, allows the lecturer to search for marks or any relevant information */}
+          {/*search input is controlled by the search query state */}
+          <input type="text" 
+          className = 'search-bar'
+          placeholder = 'search here...'
+          value = {searchQuery} /*bind the input value to the state */
+          onChange = {handleSearchChange}
+          />
+
+          {/*logout button, logs the lecturer out if clicked */}
+          <button className = 'logout-button' onClick = {handleLogout}>Logout</button>
+
+          {/*main content of the dashboard */}
+          
+
+        </div>
+
+      </header>
+
+      {/*main content of the dashboard */}
+      <h3>Lecturer Dashboard</h3>
       <p>Manage student academic issues related to your courses.</p>
       <IssueCard title="Grade Correction Request" status="In Progress" />
     </DashboardLayout>
