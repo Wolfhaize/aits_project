abbcefe9afb7b7b274e1463a2bae4b3cbb5042bf
Zoe691 <zoetino07@gmail.com>
2025-03-28 01:06:29 +0300

    Added css to registrar's dashboard

--- FRONTEND/src/pages/dashboards/registrar/RegistrarDashboard.jsx
+++ FRONTEND/src/pages/dashboards/registrar/RegistrarDashboard.jsx
@@ -1,6 +1,9 @@
 import DashboardLayout from "../../../layouts/DashboardLayout";
-import IssueCard from "../../../components/IssueCard";
+
 import React, {useEffect, useState} from 'react';
+import "../../../css/pagecss/registrardashboard.css";
+
+
 
 
 const RegistrarDashboard = () => {
@@ -26,20 +29,22 @@ const RegistrarDashboard = () => {
   return (
     <DashboardLayout role="registrar">
       <h2>Registrar Dashboard</h2>
-      <div style={{display:"flex",gap:"20px",marginTop:"20px"}}>
-        <div>
-          <h3>Total Issues</h3>
-          <p>{stats.total}</p>
-        </div>
-        <div>
-          <h3>Resolved Issues</h3>
-          <p>{stats.resolved}</p>
-        </div>
-        <div>
-          <h3>Pending Issues</h3>
-          <p>{stats.pending}</p>
-        </div>
+      <div style={{display:"flex", gap:"20px", marginTop:"20px"}}>
+      <div className="stat-box">
+        <h3>Total Issues</h3>
+        <p>{stats.total}</p>
       </div>
+      <div className="stat-box">
+        <h3>Resolved Issues</h3>
+        <p>{stats.resolved}</p>
+      </div>
+      <div className="stat-box">
+        <h3>Pending Issues</h3>
+        <p>{stats.pending}</p>
+      </div>
+      </div>
+     
+
     </DashboardLayout>
 
     
