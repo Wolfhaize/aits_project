c33fd327130c34cc171af9ab66a6ccfdcefe509f
Rita Akankunda <akankundarita04@gmail.com>
2025-03-31 09:35:32 +0300

    Update admin.py

--- BACKEND/notifications/admin.py
+++ BACKEND/notifications/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Notification, AuditLog
+from .models import Notification
 # Register your models here.# admin.py
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
@@ -7,8 +7,3 @@ class NotificationAdmin(admin.ModelAdmin):
     search_fields = ("user__username", "message")
     list_filter = ("is_read", "created_at")
 
-@admin.register(AuditLog)
-class AuditLogAdmin(admin.ModelAdmin):
-    list_display = ("user", "action", "timestamp")
-    search_fields = ("user__username", "action")
-    list_filter = ("timestamp",)
