21a39e072f6b935beca2defa8e7160bb18b7bd83
Rita Akankunda <akankundarita04@gmail.com>
2025-03-31 09:33:28 +0300

    Update views.py

--- BACKEND/notifications/views.py
+++ BACKEND/notifications/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
-from .models import Notification, AuditLog
-from .serializers import NotificationSerializer, AuditLogSerializer
+from .models import Notification
+from .serializers import NotificationSerializer
 
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
@@ -11,13 +11,3 @@ class NotificationViewSet(viewsets.ModelViewSet):
         # Return notifications for the logged-in user
         return self.queryset.filter(user=self.request.user)
 
-class AuditLogViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = AuditLogSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        # Optionally filter logs for the logged-in user
-        user = self.request.user
-        if user.is_superuser:
-            return AuditLog.objects.all()  # Admin can see all logs
-        return AuditLog.objects.filter(user=user)  # Regular users see their own logs
