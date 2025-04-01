8e9d128c5b1ec52e70ff48977ec76b26a0a17af9
creeper-byte <rodneycalvin33@gmail.com>
2025-04-01 19:59:47 +0300

    updated ATIS/settings.py

--- ATIS/settings.py
+++ ATIS/settings.py
@@ -125,4 +125,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
