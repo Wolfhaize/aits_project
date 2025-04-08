14563ef19539da9612f322148ee14eb6a19e3bef
NicoleMariah186 <nicolemariah256@gmail.com>
2025-04-08 06:54:58 +0300

    created new database

--- BACKEND/SDP/settings.py
+++ BACKEND/SDP/settings.py
@@ -53,12 +53,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "SDP.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
