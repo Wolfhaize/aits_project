4a43b83d13e986fab8b36399ef7a74a0509beb7a
RitaAkankunda <akankundarita04@gmail.com>
2025-04-10 00:32:56 +0300

    lecturer and registrar features added

--- BACKEND/SDPapp/urls.py
+++ BACKEND/SDPapp/urls.py
@@ -8,7 +8,9 @@ router.register(r'issues', views.IssueViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('issues/<int:issue_id>/assign/', views.AssignIssueView.as_view(), name='assign-issue'),
-    path('issues/<int:issue_id>/resolve/', views.ResolveIssueView.as_view(), name='resolve-issue'),
     path('', include('accounts.urls', namespace='accounts')),
-    ]
+    path('issues/<int:issue_id>/assign/', views.AssignIssueView.as_view(), name='assign-issue'),  
+    path('issues/<int:issue_id>/resolve/', views.ResolveIssueView.as_view(), name='resolve-issue'),  
+    path('issues/<int:issue_id>/lecturer-resolve/', views.LecturerResolveIssueView.as_view(), name='lecturer-resolve-issue'),
+
+]
