00871d377ab7af938a13d10aac6acc84b3133693
Haize <92583384+Wolfhaize@users.noreply.github.com>
2025-04-13 11:56:36 +0300

    Update api.js

--- FRONTEND/src/api/api.js
+++ FRONTEND/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api"; // Adjust if necessary
+const BASE_URL = "http://127.0.0.1:8000/api"; 
 
 // Login function to authenticate the user
 export const login = async (email, password) => {
