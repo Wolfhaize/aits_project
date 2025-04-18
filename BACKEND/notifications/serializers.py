35f89759acf2e6293156d74eae15b3aaa2eb704c
creeper-byte <rodneycalvin33@gmail.com>
2025-04-19 02:28:20 +0300

    updated the notifications serializers.py

--- BACKEND/notifications/serializers.py
+++ BACKEND/notifications/serializers.py
@@ -3,6 +3,5 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id', 'user', 'message', 'is_read', 'created_at']
-        read_only_fields = ['user', 'created_at'] 
-
+        fields = ['id', 'user', 'notification_type', 'priority', 'title', 'message', 'is_read', 'created_at', 'sent_at', 'content_type', 'object_id', 'related_object']
+        read_only_fields = ['user', 'created_at', 'sent_at', 'content_type', 'object_id', 'related_object'] 
