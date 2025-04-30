1d5319f06ea57381c1515a21d463aa2b70242408
RitaAkankunda <akankundarita04@gmail.com>
2025-04-30 18:52:27 +0300

    database changes to postgress

--- BACKEND/SDP/settings.py
+++ BACKEND/SDP/settings.py
@@ -61,12 +61,18 @@ TEMPLATES = [
 WSGI_APPLICATION = "SDP.wsgi.application"
 
 
+# Database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'GroupC'),
+        'USER': os.environ.get('DB_USER', 'myuser'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '2003'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
